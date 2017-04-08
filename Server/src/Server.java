90f0fd4e081cdd145d5f35f8d4fb09e973baf4d9
NathanLiccardo <nathan.liccardo@ulb.ac.be>
2017-04-08 11:55:06 +0200

    OK

--- Server/src/Server.java
+++ Server/src/Server.java
@@ -4,32 +4,31 @@
  * and open the template in the editor.
  */
 
-import Controller.ConnectionController;
 
 
+import Controller.NodeController;
+import Controller.SSLController;
 import Model.NodeModel;
 import java.io.IOException;
-import java.net.InetAddress;
-import java.net.ServerSocket;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import javax.net.ssl.SSLServerSocket;
+import javax.net.ssl.SSLSocket;
 
 /**
  *
  * @author Nathan
  */
 public class Server implements Runnable{
+    private final int _port;
+    private final String _host ;
+    //private ServerSocket _server;
+    public ArrayList<NodeModel> _node;
     
-    private final int port = 2000;
-    private final String host = "localhost";
-    private ServerSocket server = null;
-    public ArrayList<NodeModel> node = new ArrayList<>();
-    
+    /*
     public void initServer() {
         try {
-            server = new ServerSocket(port, 100,InetAddress.getByName(host));
+            _server = new ServerSocket(_port, 100,InetAddress.getByName(_host));
         } catch (UnknownHostException ex) {
             Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
         } catch (IOException ex) {
@@ -42,15 +41,46 @@ public class Server implements Runnable{
         this.initServer();
         while (true){
             try {
-                new Thread(new ConnectionController(server.accept(), port,node)).start();
+                new Thread(new NodeController(_server.accept(), _port,_node)).start();
             } catch (IOException ex) {
                 System.err.println(ex);
             }
         }
+    }*/
+    
+    private SSLServerSocket initServer() throws UnknownHostException,IOException {
+        SSLController sslConnection = new SSLController(_port, 100, _host);
+        sslConnection.connect();
+        return sslConnection.getServer();
+    }
+    
+    @Override
+    public void run() {
+        try {
+            SSLServerSocket server = this.initServer();
+            while (true) {
+                SSLSocket socket = (SSLSocket) server.accept();
+                new Thread(new NodeController(socket,_port, _node)).start();
+            }
+        } catch (IOException ex) {
+            System.err.println(ex);
+        }
+    }
+    
+    public Server(){
+        _host = "localhost";
+        _port = 2000;
+        _node = new ArrayList<>();
+    }
+    
+    public Server(String host, int port){
+        _host = host;
+        _port = port;
+        _node = new ArrayList<>();
     }
     
     public static void main(String args[]) throws Exception{
-        new Thread(new Server()).start();
+        new Thread(new Server(args[0],Integer.getInteger(args[1]))).start();
     }
     
 }
